26deef6672aa7f8a1893c8be3ae6a351d03a1df6
Glitch (hello-flask) <none>
2024-04-09 15:04:46 +0000

    ⛪️⛑ Checkpoint ./server.py:5501376/1315

--- server.py
+++ server.py
@@ -2,44 +2,18 @@ import os
 from flask import Flask, request, render_template, jsonify
 from requests import get, post
 import json
-import sqlite3
-import click
-from flask import current_app, g
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
 token = os.environ.get('TOKEN')
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            "user_data.sql",
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-        
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-        
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-    
-def create_app():
-  init_app(app)
-  return app
+@app.route('/admin/getDB')
+def get_db_command():
+    return get_db()
 
 @app.route('/admin/initDB')
 def init_db_command():
