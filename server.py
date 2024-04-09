046ae6ab58344ec7826dec4aa61bb4c62c8d9168
Glitch (hello-flask) <none>
2024-04-09 14:42:10 +0000

    🌎📫 Checkpoint ./server.py:5501376/673 ./requirements.txt:5501376/14 ./schema.sql:5501376/202

--- server.py
+++ server.py
@@ -2,10 +2,36 @@ import os
 from flask import Flask, request, render_template, jsonify
 from requests import get, post
 import json
+import sqlite3
+import click
+from flask import current_app, g
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
 token = os.environ.get('TOKEN')
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+        
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
 
 @app.route('/')
 def homepage():
