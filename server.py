ef68e0a3df6999eb086e9f0bd5ed38f91ceef2f7
Glitch (hello-flask) <none>
2024-04-10 12:10:43 +0000

    📌📯 Checkpoint ./server.py:5501376/1262

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from requests import get, post
 import json
 import sqlite3
 import pandas as pd
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
@@ -40,6 +40,8 @@ def get_db():
 
 @app.route('/v1/admin/createDB')
 def create_db():
+    if request.args['secret']!=adm_pass:
+      abort(401)
     conn = sqlite3.connect('test_database') 
     c = conn.cursor()
     c.execute('''
@@ -53,20 +55,40 @@ def create_db():
           ''')
     conn.commit()
     return "ok"
+
+@app.route('/v1/admin/deleteRecord')
+def deleteRecord():
+    if request.args['secret']!=adm_pass:
+      abort(401)
+    try:
+        sqliteConnection = sqlite3.connect('test_database')
+        cursor = sqliteConnection.cursor()
+        sql_delete_query = '''DELETE from SqliteDb_developers where id = "''' + request.args['id'] 
+        cursor.execute(sql_delete_query)
+        sqliteConnection.commit()
+        print("Record deleted successfully ")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to delete record from sqlite table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            return "the sqlite connection is closed"
         
 @app.route('/')
 def homepage():
     """Displays the homepage."""
     return render_template('index.html')
   
-@app.route('/v1/postFeedback', methods=['POST'])
+@app.route('/v1/feedback/postFeedback', methods=['POST'])
 def postFeedback():
     data = json.loads(request.data)
     message = "Feedback from: " + data["name"] + "\n\n" + "E-mail: " + data["email"] + "\n\n" + data["feedback"]
     req = get('https://api.telegram.org/bot' + token + '/sendMessage?chat_id=468110974&parse_mode=Markdown&text=' + message)
     return req.content, req.status_code
   
-@app.route('/v1/addUser', methods=['POST'])
+@app.route('/v1/users/addUser', methods=['POST'])
 def addUser():
     data = json.loads(request.data)
     conn = sqlite3.connect('test_database') 
@@ -80,7 +102,11 @@ def addUser():
           ''')
     conn.commit()
     return "ok"
-    
+  
+@app.route('/v1/users/authorize', methods=['POST'])
+def authorize():
+  
+    return "ok"
 
 if __name__ == '__main__':
     app.run()
