ca4697576e7e412939b8fde0ba4fda028d7d80d0
Glitch (hello-flask) <none>
2024-04-24 13:59:38 +0000

    🔖🏋 Checkpoint ./server.py:5501376/10020

--- server.py
+++ server.py
@@ -17,27 +17,8 @@ def after_request(response):
     # Other headers can be added here if needed
     return response
   
-
-token = os.environ.get('TOKEN')
 adm_pass = os.environ.get('ADMIN_SECRET')
 auth_secret = os.environ.get('AUTH_SECRET')
-
-@app.route('/v1/admin/getSocialDB')
-def get_social_db():
-    if request.args['secret']!=adm_pass:
-      abort(401)
-    conn = sqlite3.connect('database_social') 
-    c = conn.cursor()
-    c.execute('''
-          SELECT
-          id,
-          name,
-          text
-          FROM comments
-          ''')
-    df = pd.DataFrame(c.fetchall(), columns=['id','name','text'])
-    conn.commit()
-    return df.to_string(index=False).replace('\n', '<br>')
   
 @app.route('/v1/admin/getDB')
 def get_db():
@@ -75,22 +56,6 @@ def create_db():
           ''')
     conn.commit()
     return "ok"
-  
-@app.route('/v1/admin/createSocialDB')
-def create_social_db():
-    if request.args['secret']!=adm_pass:
-      abort(401)
-    conn = sqlite3.connect('database_social') 
-    c = conn.cursor()
-    c.execute('''
-          CREATE TABLE comments (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          text TEXT NOT NULL
-          );
-          ''')
-    conn.commit()
-    return "ok"
 
 @app.route('/v1/admin/deleteRecord')
 def deleteRecord():
@@ -117,19 +82,12 @@ def homepage():
     """Displays the homepage."""
     return render_template('index.html')
   
-@app.route('/v1/feedback/postFeedback', methods=['POST'])
-def postFeedback():
-    data = json.loads(request.data)
-    message = "Feedback from: " + data["name"] + "\n\n" + "E-mail: " + data["email"] + "\n\n" + data["feedback"]
-    req = get('https://api.telegram.org/bot' + token + '/sendMessage?chat_id=468110974&parse_mode=Markdown&text=' + message)
-    return req.content, req.status_code
-  
 @app.route('/v1/users/addUser', methods=['POST'])
 def addUser():
     data = json.loads(request.data)
     conn = sqlite3.connect('test_database') 
     c = conn.cursor()
-    pwd = generate_password_hash(data['password'], "sha256")
+    pwd = generate_password_hash(data['password'], "pbkdf2:sha256")
     try:
       c.execute('''
           INSERT INTO users (firstname, lastname, email, password)
@@ -168,34 +126,6 @@ def authorize():
 def getUser():
     auth = request.headers.get('Authorization')
     return jsonify(jwt.decode(auth, auth_secret, algorithms=["HS256"]))
-  
-@app.route('/v1/social/saveComment', methods=['POST'])
-def saveComment():
-    data = json.loads(request.data)
-    conn = sqlite3.connect('database_social') 
-    c = conn.cursor()
-    c.execute('''
-          INSERT INTO comments (name, text)
-                VALUES
-                ("''' + data['name'] + '''","''' + data['text'] + '''")
-          ''')
-    conn.commit()
-    return "ok"
-  
-@app.route('/v1/social/getComments', methods=['GET'])
-def getComments():
-    conn = sqlite3.connect('database_social') 
-    c = conn.cursor()
-    c.execute('''
-          SELECT * FROM comments ORDER BY id DESC LIMIT 20;
-          ''')
-    dict = {}
-    data = c.fetchall()
-    i = 0
-    for entity in data:
-      dict[i] = {"name":entity[1],"text":entity[2]}
-      i += 1
-    return jsonify(dict)
     
 
 if __name__ == '__main__':
